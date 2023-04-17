d52844e83796191d2c8cc715ee0002d0752a6156
jaewook90 <kimjaetube90@gmail.com>
2023-04-17 12:25:55 +0900

    호텔 체크인 모듈 작업 / 김재욱, 오수환

---
+++ lib/Modals/HotelModules/BellBoyYNModalFinal.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:kori_wis_demo/Providers/NetworkModel.dart';
+import 'package:kori_wis_demo/Providers/ServingModel.dart';
+import 'package:kori_wis_demo/Screens/Services/Hotel/HotelServiceMenuFinal.dart';
+import 'package:kori_wis_demo/Screens/Services/Navigation/NavigatorProgressModuleFinal.dart';
+import 'package:kori_wis_demo/Utills/navScreens.dart';
+import 'package:provider/provider.dart';
+
+class BellBoyYNModalFinal extends StatefulWidget {
+  const BellBoyYNModalFinal({Key? key}) : super(key: key);
+
+  @override
+  State<BellBoyYNModalFinal> createState() => _BellBoyYNModalFinalState();
+}
+
+class _BellBoyYNModalFinalState extends State<BellBoyYNModalFinal> {
+  late ServingModel _servingProvider;
+  late NetworkModel _networkProvider;
+
+  late String countDownPopup;
+
+  @override
+  Widget build(BuildContext context) {
+    _servingProvider = Provider.of<ServingModel>(context, listen: false);
+    _networkProvider = Provider.of<NetworkModel>(context, listen: false);
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    countDownPopup = 'assets/screens/Hotel/koriZFinalBellBoyPop.png';
+
+    return Container(
+        padding: EdgeInsets.only(top: 607),
+      child:AlertDialog(
+        alignment: Alignment.topCenter,
+        content: Stack(
+          children: [
+            Container(
+            width: 740,
+            height: 362,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(countDownPopup), fit: BoxFit.fill)),
+            child: null,
+          ),
+            Positioned(
+              left: 0,
+              top: 242,
+              child: FilledButton(
+                style: FilledButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shape: RoundedRectangleBorder(
+                        // side: BorderSide(width: 1, color: Colors.redAccent),
+                        borderRadius:
+                        BorderRadius.circular(0)),
+                    fixedSize: Size(370, 120)),
+                onPressed: (){
+                  navPage(context: context, page: HotelServiceMenu(), enablePop: false).navPageToPage();
+                },
+                child: null,
+              ),
+            ),
+            Positioned(
+              left: 370,
+              top: 242,
+              child: FilledButton(
+                style: FilledButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shape: RoundedRectangleBorder(
+                        // side: BorderSide(width: 1, color: Colors.redAccent),
+                        borderRadius:
+                        BorderRadius.circular(0)),
+                    fixedSize: Size(370, 120)),
+                onPressed: (){
+                  navPage(context: context, page: HotelServiceMenu(), enablePop: false).navPageToPage();
+                },
+                child: null,
+              ),
+            ),
+          ]
+        ),
+        backgroundColor: Colors.transparent,
+        contentTextStyle: Theme.of(context).textTheme.headlineLarge,
+        // actionsPadding: EdgeInsets.only(top: screenHeight * 0.001),
+      )
+    );
+  }
+}
--- lib/Modals/OrderModules/NFCModalFinal.dart
+++ lib/Modals/OrderModules/NFCModalFinal.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:kori_wis_demo/Providers/NetworkModel.dart';
+import 'package:kori_wis_demo/Screens/Services/Hotel/HotelServiceMenuFinal.dart';
+import 'package:kori_wis_demo/Screens/Services/Hotel/HotelServiceRoomReceipt.dart';
+import 'package:kori_wis_demo/Utills/navScreens.dart';
+import 'package:provider/provider.dart';
 
 class NFCModuleScreenFinal extends StatefulWidget {
   const NFCModuleScreenFinal({Key? key}) : super(key: key);
@@ -8,93 +13,90 @@ class NFCModuleScreenFinal extends StatefulWidget {
 }
 
 class _NFCModuleScreenFinalState extends State<NFCModuleScreenFinal> {
+  late NetworkModel _networkProvider;
   String NFCimg = 'assets/screens/Serving/koriZFinalNFC.png';
 
   @override
   Widget build(BuildContext context) {
+    _networkProvider = Provider.of<NetworkModel>(context, listen: false);
 
-    return Container(
-        padding: EdgeInsets.only(top: 100),
-        decoration: BoxDecoration(
-          border: Border.fromBorderSide(BorderSide(color: Colors.white)),),
-        child: Dialog(
-          alignment: Alignment.topCenter,
-          backgroundColor: Colors.transparent,
-          // backgroundColor: Color(0xff000000),
-          shape: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(0),
-              borderSide: BorderSide(
-                color: Color(0xFFB7B7B7),
-                style: BorderStyle.solid,
-                width: 1,
-              )),
-          child: Container(
-            height: 1401,
-            width: 1072.5,
-            child: Stack(children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.fromBorderSide(BorderSide(width: 2, color: Colors.white)),
-                  image: DecorationImage(image: AssetImage(NFCimg)),
-            ),
-        ),
-              Positioned(
-                  left: 53,
-                  top: 35,
-                  child: Container(
-                    width: 48,
-                    height: 48,
-                    color: Colors.transparent,
-                    child: FilledButton(
-                      style: FilledButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          shape: RoundedRectangleBorder(
-                              side: BorderSide(width: 1, color: Colors.white),
-                              borderRadius: BorderRadius.circular(0))
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: null,
-                    ),
-                  )),
-              Positioned(
-                  left: 890,
-                  top: 35,
-                  child: Container(
-                    width: 48,
-                    height: 48,
-                    color: Colors.transparent,
-                    child: FilledButton(
-                      style: FilledButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          shape: RoundedRectangleBorder(
-                              side: BorderSide(width: 1, color: Colors.white),
-                              borderRadius: BorderRadius.circular(0))
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                        Navigator.pop(context);
-                        Navigator.pop(context);
-                        Navigator.pop(context);
-                      },
-                      child: null,
-                    ),
-                  )),
-              Positioned(
-                left: 500,
-                height: 500,
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  child: FilledButton(
-                    onPressed: (){},
-                    child: Container(),
-                  ),
-                ),
-              )
-      ]),
+    return GestureDetector(
+      onTap: (){
+        if(_networkProvider.serviceState == 2){
+          navPage(context: context, page: HotelRoomReceipt(), enablePop: false).navPageToPage();
+        }
+      },
+      child: Container(
+          padding: EdgeInsets.only(top: 100),
+          decoration: BoxDecoration(
+            border: Border.fromBorderSide(BorderSide(color: Colors.white)),),
+          child: Dialog(
+            alignment: Alignment.topCenter,
+            backgroundColor: Colors.transparent,
+            // backgroundColor: Color(0xff000000),
+            shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(0),
+                borderSide: BorderSide(
+                  color: Color(0xFFB7B7B7),
+                  style: BorderStyle.solid,
+                  width: 1,
+                )),
+            child: Container(
+              height: 1401,
+              width: 1072.5,
+              child: Stack(children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.fromBorderSide(BorderSide(width: 2, color: Colors.white)),
+                    image: DecorationImage(image: AssetImage(NFCimg)),
+              ),
           ),
-    ));
+                Positioned(
+                    left: 53,
+                    top: 35,
+                    child: Container(
+                      width: 48,
+                      height: 48,
+                      color: Colors.transparent,
+                      child: FilledButton(
+                        style: FilledButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(width: 1, color: Colors.white),
+                                borderRadius: BorderRadius.circular(0))
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: null,
+                      ),
+                    )),
+                Positioned(
+                    left: 890,
+                    top: 35,
+                    child: Container(
+                      width: 48,
+                      height: 48,
+                      color: Colors.transparent,
+                      child: FilledButton(
+                        style: FilledButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(width: 1, color: Colors.white),
+                                borderRadius: BorderRadius.circular(0))
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                        },
+                        child: null,
+                      ),
+                    )),
+        ]),
+            ),
+      )),
+    );
   }
 }
